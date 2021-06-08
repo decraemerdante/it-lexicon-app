018366021e79ea731f28f74ddfa78b7175629147
decraemerdante <decraemerdante@gmail.com>
2021-06-08 16:43:48 +0200

    init service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleClient, ArticleDto } from './shared/API';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'it-lexicon-app';
+
+  articles: ArticleDto[]
+  constructor(private articleClient: ArticleClient){}
+
+  ngOnInit(): void {
+    this.articleClient.getAll().subscribe(data => console.log(data));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArticleClient, CategoryClient } from './shared/API';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ArticleClient, CategoryClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
